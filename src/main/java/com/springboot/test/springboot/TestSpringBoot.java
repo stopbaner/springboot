292f81b44bea7582de25e3c9fa3e6991f228b286
itheima <bos@itheima.cn>
2020-05-23 22:20:57 +0800

    shenzp-创建springboot

---
+++ src/main/java/com/springboot/test/springboot/TestSpringBoot.java
@@ -0,0 +1,21 @@
+package com.springboot.test.springboot;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @描述
+ * @创建人 szp
+ * @修改人和其它信息
+ */
+@RestController
+//@SpringBootApplication
+@EnableAutoConfiguration
+public class TestSpringBoot {
+    @RequestMapping("/hello")
+    public String sendString(){
+        return "helloworld";
+    }
+}
---
+++ src/main/java/com/springboot/test/springboot/controller/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.test.springboot.controller;
+
+import com.springboot.test.springboot.TestSpringBoot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBoot.class, args);
+    }
+
+}
